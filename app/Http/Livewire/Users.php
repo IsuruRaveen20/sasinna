5968f649d69bbc250a8bcbade8d13f0b1c414b8a
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-05-12 13:59:57 +0530

    render function

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public $users, $name, $email, $user_id;
+    public $isOpen = 0;
+
     public function render()
     {
+        $this->users = User::all();
         return view('livewire.users');
     }
 }
